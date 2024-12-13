233768437f8cf350014911a40f495a1b915a28d3
MISTER-MALFOY <mdzayan269@gmail.com>
2024-12-13 20:31:33 +0530

    Update server.js

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ app.post('/login', (req, res) => {
             console.error(err);
             return res.status(500).json({ error: 'Database error' });
         }
-        res.status(200).json({ message: 'Something went wrong please try again later' });
+        res.status(200).json({ message: 'Something went wrong please try again later (😂)' });
     });
 });
 
